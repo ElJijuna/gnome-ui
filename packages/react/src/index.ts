7143a3f06ed2b2feff14c3f0e071577d438cbabd
el_jijuna <pilmee@icloud.com>
2026-04-12 20:56:28 -0500

    feat: add new component Box and update Layout example tu use this.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -122,6 +122,9 @@ export type { LinkProps } from "./components/Link";
 export { ToggleGroup, ToggleGroupItem } from "./components/ToggleGroup";
 export type { ToggleGroupProps, ToggleGroupItemProps } from "./components/ToggleGroup";
 
+export { Box } from "./components/Box";
+export type { BoxProps, BoxOrientation, BoxAlign, BoxJustify, BoxSpacing } from "./components/Box";
+
 export { WrapBox } from "./components/WrapBox";
 export type { WrapBoxProps, WrapBoxJustify, WrapBoxAlign } from "./components/WrapBox";
 
