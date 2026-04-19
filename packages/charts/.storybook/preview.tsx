653fb4390284fe547440b45b226f75325be5f99d
el_jijuna <pilmee@icloud.com>
2026-04-19 12:36:23 -0500

    chore: update all storybooks configs to override viewport options to Mobile Portrait, Mobile Landscape, Tablet Portrait, Table Landscape, Desktop and Large Desktop (closes #31)

--- packages/charts/.storybook/preview.tsx
+++ packages/charts/.storybook/preview.tsx
@@ -46,6 +46,16 @@ const preview: Preview = {
         { name: "gnome-dark", value: "#242424" },
       ],
     },
+    viewport: {
+      options: {
+        mobilePortrait:  { name: "Mobile Portrait",  styles: { width: "390px",  height: "844px"  }, type: "mobile"  },
+        mobileLandscape: { name: "Mobile Landscape",  styles: { width: "844px",  height: "390px"  }, type: "mobile"  },
+        tabletPortrait:  { name: "Tablet Portrait",  styles: { width: "768px",  height: "1024px" }, type: "tablet"  },
+        tabletLandscape: { name: "Tablet Landscape",  styles: { width: "1024px", height: "768px"  }, type: "tablet"  },
+        desktop:         { name: "Desktop",          styles: { width: "1280px", height: "800px"  }, type: "desktop" },
+        largeDesktop:    { name: "Large Desktop",    styles: { width: "1920px", height: "1080px" }, type: "desktop" },
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
