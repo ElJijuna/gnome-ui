b30b775d341afae80413cffecd590f6781ebe9e7
el_jijuna <pilmee@icloud.com>
2026-04-22 07:30:30 -0500

    feat: uodate SIdebarSection for support Icon, and collapsible and expand functions (closes  #34)

--- packages/react/src/components/Sidebar/index.ts
+++ packages/react/src/components/Sidebar/index.ts
@@ -2,7 +2,7 @@ export { Sidebar, SidebarCollapsedContext, useSidebarCollapsed } from "./Sidebar
 export type { SidebarProps } from "./Sidebar";
 
 export { SidebarSection } from "./SidebarSection";
-export type { SidebarSectionProps } from "./SidebarSection";
+export type { SidebarSectionProps, SidebarSectionHandle } from "./SidebarSection";
 
 export { SidebarItem } from "./SidebarItem";
 export type { SidebarItemProps, SidebarMenuEntry } from "./SidebarItem";
