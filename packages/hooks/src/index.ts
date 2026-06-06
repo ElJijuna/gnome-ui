f4f09bcb74fa8c5fd924a2436b35486c87ba8b16
ElJijuna <pilmee@icloud.com>
2026-06-06 18:49:13 -0500

    fix(@gnome-ui/hooks): update export statements for useHapticFeedback for clarity

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,5 +1,9 @@
 export { type BreakpointInfo, useBreakpoint } from './useBreakpoint';
+export {
+  type GnomeHapticEvent,
+  type UseHapticFeedbackResult,
+  useHapticFeedback,
+} from './useHapticFeedback';
 export { useNativeEvent } from './useNativeEvent';
-export { type GnomeHapticEvent, type UseHapticFeedbackResult, useHapticFeedback } from './useHapticFeedback';
 export { type PlatformInfo, usePlatform } from './usePlatform';
 export { useRuntime } from './useRuntime';
